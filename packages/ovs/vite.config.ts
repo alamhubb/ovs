af2f1d3081527d42dc6c490bac313e45c7d77571
socialuni <socialuni@163.com>
2024-09-29 01:29:14 +0800

    support simple syntax ``` div{   123 } ```

--- packages/ovs/vite.config.ts
+++ packages/ovs/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // import vuePlugin from "./plugin-vue/src";
 import Inspect from 'vite-plugin-inspect'
 import type {Plugin} from 'vite'
-import vitePluginOvs from "./src/pluginOvs/vitePluginOvs";
+import vitePluginOvs from "./src/ovs/vitePluginOvs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
