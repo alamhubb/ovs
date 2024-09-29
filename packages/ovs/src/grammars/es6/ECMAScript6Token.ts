3ccb0cf197c620015043a4f485ecc3735f9f8608
socialuni <socialuni@163.com>
2024-09-29 16:17:19 +0800

    delete redundant code

--- packages/ovs/src/grammars/es6/ECMAScript6Token.ts
+++ packages/ovs/src/grammars/es6/ECMAScript6Token.ts
@@ -1,7 +1,21 @@
 import {createToken} from "chevrotain";
-import {AbsKeyword} from "../ecma5/ecma5_tokens";
+import {AbsKeyword, Question} from "../ecma5/ecma5_tokens";
 
 export * from "../ecma5/ecma5_tokens";
+
+
+export enum ECMAScript6TokenName {
+    ConstTok = 'ConstTok',
+    ClassTok = 'ClassTok',
+    ExtendsTok = 'ExtendsTok',
+    ExportTok = 'ExportTok',
+    ImportTok = 'ImportTok',
+
+    Arrow = 'Arrow',
+    BackTick = 'BackTick',
+    DollarCurly = 'DollarCurly',
+    Ellipsis = 'Ellipsis',
+}
 
 // ES6 新增的标记
 
@@ -12,25 +26,24 @@ export * from "../ecma5/ecma5_tokens";
 // export const FromTok = createToken({name: "FromTok", pattern: /from/, categories: AbsKeyword});
 // export const AsTok = createToken({name: "AsTok", pattern: /as/, categories: AbsKeyword});
 //已使用
-export const ConstTok = createToken({name: "ConstTok", pattern: /const/, categories: AbsKeyword});
-export const ClassTok = createToken({name: "ClassTok", pattern: /class/, categories: AbsKeyword});
-export const ExtendsTok = createToken({name: "ExtendsTok", pattern: /extends/, categories: AbsKeyword});
-export const ExportTok = createToken({name: "ExportTok", pattern: /export/, categories: AbsKeyword});
-export const ImportTok = createToken({name: "ImportTok", pattern: /import/, categories: AbsKeyword});
+export const ConstTok = createToken({name: ECMAScript6TokenName.ConstTok, categories: AbsKeyword});
+export const ClassTok = createToken({name: ECMAScript6TokenName.ClassTok, categories: AbsKeyword});
+export const ExtendsTok = createToken({name: ECMAScript6TokenName.ExtendsTok, categories: AbsKeyword});
+export const ExportTok = createToken({name: ECMAScript6TokenName.ExportTok, categories: AbsKeyword});
+export const ImportTok = createToken({name: ECMAScript6TokenName.ImportTok, categories: AbsKeyword});
 
 
 // 箭头函数
-export const Arrow = createToken({name: "Arrow", pattern: /=>/});
+export const Arrow = createToken({name: ECMAScript6TokenName.Arrow, categories: Question});
 
 // 模板字符串
-export const BackQuote = createToken({name: "BackTick", pattern: /`/});
-export const DollarCurly = createToken({name: "DollarCurly", pattern: /\${/});
+export const BackQuote = createToken({name: ECMAScript6TokenName.BackTick, categories: Question});
+export const DollarCurly = createToken({name: ECMAScript6TokenName.DollarCurly, categories: Question});
 //未使用
 // export const TemplateCharacters = createToken({name: "TemplateCharacters", pattern: /[^`$]+/});
 
 // 扩展运算符
-export const Ellipsis = createToken({name: "Ellipsis", pattern: /\.\.\./});
-
+export const Ellipsis = createToken({name: ECMAScript6TokenName.Ellipsis, categories: Question});
 
 
 // (	parentheses
