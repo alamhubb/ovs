97892056358e9825f3e3070e79ff7dfed959dd58
socialuni <socialuni@163.com>
2024-10-06 02:14:39 +0800

    support ```import {h} from "vue";            import toVueComponent from "@/components/ToVueComponent";
               class Testsyntax11 {
                 static render() {
                   return h('div', 123456)
                 }
               }
    
               export default toVueComponent(Testsyntax11)```

--- packages/ovs/src/components/TestOvs.vue
+++ packages/ovs/src/components/TestOvs.vue
@@ -9,4 +9,15 @@ class Testsyntax11 {
 }
 
 export default toVueComponent(Testsyntax11)
+
+
+//问题是，对象不支持装饰器
+// const ab {
+//   av:string = '100'
+//   av:string
+// }
+//
+// 语法和类一样。
+//
+// 编译后什么样子呢。
 </script>
