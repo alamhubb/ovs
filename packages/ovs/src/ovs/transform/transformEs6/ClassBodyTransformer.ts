c94008697ad2dbf0efa1a26c93bb9d30098ea1e3
socialuni <socialuni@163.com>
2024-10-06 01:24:34 +0800

    support ```export default class Testsyntax {                static render(){                    return div{                        123456                    }                }            }```

--- packages/ovs/src/ovs/transform/transformEs6/ClassBodyTransformer.ts
+++ packages/ovs/src/ovs/transform/transformEs6/ClassBodyTransformer.ts
@@ -6,7 +6,6 @@ export default class ClassBodyTransformer {
     //return  members
     static transformClassBody(classBody: ChevrotainEcma5Ast) {
         let members = []
-
         for (const child of classBody.children) {
             if (child.name === Es6SyntaxName.ClassElement) {
                 let member = Es6Transformer.transform(child)
